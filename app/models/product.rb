f4d79b4f1dfa257912d05101cafbce6ce3a79db2
Megan Byrne <megan.byrne27@gmail.com>
2014-04-19 15:07:48 -0700

    Add Product Validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,10 @@
 class Product < ActiveRecord::Base
+  
+  validates :title, uniqueness: true, presence: true
+  validates :description, presence: true
+  validates :image_url, presence: true, allow_blank: true, format: {
+    with:     %r{\.(gif|jpg|png)\Z}i,
+    message:  'must be a url for GIF, JPG, of PNG image.'
+  }
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
 end
